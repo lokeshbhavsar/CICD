dde77c7c75f77b17679c04cd97a29afbdac6f164
lokeshbhavsar <lokesh.bhavsar127@gmail.com>
2026-03-28 23:21:22 +0530

    fix:on sucess

--- js/test.js
+++ js/test.js
@@ -7,9 +7,3 @@ if (sum(1, 2) === 3) {
   throw new Error("JS Test 1 Failed");
 }
 
-// ❌ fail test
-if (sum(2, 2) === 5) {
-  console.log("JS Test 2 Passed");
-} else {
-  throw new Error("JS Test 2 Failed");
-}
--- py/test.py
+++ py/test.py
@@ -6,8 +6,3 @@ if sum(1, 2) == 3:
 else:
     raise Exception("Python Test 1 Failed")
 
-# ❌ fail
-if sum(2, 2) == 5:
-    print("Python Test 2 Passed")
-else:
-    raise Exception("Python Test 2 Failed")
